8da1acb244570ff93bd95d072954b471a42e0170
Luda Belenitsa <ludmilabelenica2@gmail.com>
2023-04-04 09:04:09 +0300

    готово

---
+++ Exercise 27 (Day28)/z1/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using z1.ViewModels;
+
+namespace z1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowsViewModel();
+        }
+    }
+}
